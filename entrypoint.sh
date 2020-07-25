189ff5376f61c0f04f8afa58c0ab514532ef0460
Sébastien Brousse <seb.brousse@gmail.com>
2020-07-25 15:26:50 +0200

    Do not put token-file into workdir and volume

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-echo "$SCW_TOKEN" > token.txt
+echo "$SCW_TOKEN" > /scw-token.txt
 
-prometheus-scw-sd --output.file="./scw.json" --scw.token-file="token.txt"
+prometheus-scw-sd --output.file="./scw.json" --scw.token-file="/scw-token.txt"
